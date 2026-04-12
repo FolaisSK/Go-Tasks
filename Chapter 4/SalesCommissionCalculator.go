be9cfb81f0c590d30a5aa05cc0bdfe6b1589dbc7
Fola <folajimibabslawal@gmail.com>
2026-04-12 18:33:18 +0100

    qed

---
+++ Chapter 4/SalesCommissionCalculator.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		baseAmount         = 200
+		charge             = 0.09
+		value      float64 = 0
+		choice             = 0
+	)
+
+	for i := 0; choice != -1; i++ {
+	}
+	fmt.Println("Enter Item Value:")
+	fmt.Scan(&value)
+
+}
